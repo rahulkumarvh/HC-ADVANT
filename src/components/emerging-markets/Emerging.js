8009003c2c990ada06626d623c63fa752dd61e2a
rahulkumarvh <vhrahulkumar@gmail.com>
2021-09-09 14:50:49 +0530

    image update

--- src/components/emerging-markets/Emerging.js
+++ src/components/emerging-markets/Emerging.js
@@ -6,13 +6,39 @@ import b2 from "../../images/b2.jpeg";
 const Emerging = () => {
   return (
     <>
-      <section class="flex items-center justify-center h-80 bg-gray-800 pt-32 pb-32">
-        <div class="text-center">
-          <h2 class="mt-6 text-3xl font-bold text-white md:text-5xl">
-            Emerging Markets
-          </h2>
+      <main id="top">
+        <div
+          className="relative pt-16 pb-32 flex content-center items-center justify-center"
+          style={{
+            minHeight: "75vh",
+          }}
+        >
+          <div
+            className="absolute top-0 w-full h-full bg-center bg-cover"
+            style={{
+              backgroundImage:
+                "url('https://images.pexels.com/photos/935949/pexels-photo-935949.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260')",
+            }}
+          >
+            <span
+              id="blackOverlay"
+              className="w-full h-full absolute opacity-75 bg-black"
+            ></span>
+          </div>
+          <div className="container relative mx-auto">
+            <div className="items-center flex flex-wrap">
+              <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+                <div className="pr-12">
+                  <h1 className="text-white font-semibold text-5xl">
+                    Emerging Markets
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </section>
+      </main>
+
       <section class="bg-white">
         <div class="max-w-5xl px-6 py-16 mx-auto space-y-8 md:flex md:items-center md:space-y-0">
           <div class="md:w-2/3">
@@ -27,15 +53,13 @@ const Emerging = () => {
               ></img>
             </div>
             <h2 class="text-3xl font-semibold text-gray-800 md:mt-6">
-              Lorem ipsum dolor{" "}
+              Emerging Markets{" "}
             </h2>
             <p class="max-w-lg mt-4 text-gray-600">
-              Duis aute irure dolor in reprehenderit in voluptate velit esse
-              illum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit
-              anim id est laborum dolore eu fugiat nulla pariatur. Excepteur
-              sint occaecat cupidatat non proident, sunt in culpa qui officia
-              deserunt mollit anim id est laborum.
+              We partner with the international companies and local ones alike
+              into providing very viable soft landing business services that is
+              all weathered and bound to succeed; especially in the areas of new
+              businesses and frontiers.
             </p>
           </div>
           <div class="md:w-1/3">
